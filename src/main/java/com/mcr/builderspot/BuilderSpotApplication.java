ae0dcbcb857e730710ddb9cac92ac6d972c8cf34
Bhumit Hihoriya <bhumithihoriya@Bhumits-MacBook-Pro.local>
2023-12-02 17:19:27 +0530

    changes

---
+++ src/main/java/com/mcr/builderspot/BuilderSpotApplication.java
@@ -0,0 +1,13 @@
+package com.mcr.builderspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilderSpotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BuilderSpotApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/mcr/builderspot/BuilderSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcr.builderspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilderSpotApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
